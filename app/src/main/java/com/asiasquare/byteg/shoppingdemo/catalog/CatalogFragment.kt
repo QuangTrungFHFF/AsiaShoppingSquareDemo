355ed0318885300bd30a979972e4ab2097582f08
Tran Quang Trung <tranquangtrung.vgu@gmail.com>
2021-05-26 16:25:58 +0200

    Add adapter for catalog recyclerView

--- app/src/main/java/com/asiasquare/byteg/shoppingdemo/catalog/CatalogFragment.kt
+++ app/src/main/java/com/asiasquare/byteg/shoppingdemo/catalog/CatalogFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
 import com.asiasquare.byteg.shoppingdemo.R
 import com.asiasquare.byteg.shoppingdemo.databinding.FragmentCatalogBinding
 /**
@@ -16,6 +17,15 @@ class CatalogFragment : Fragment() {
     /** binding will only exist between onAttach and on Detach **/
     private var _binding : FragmentCatalogBinding? = null
     private val binding get() = _binding!!
+
+    /**
+     * Create viewModel, provide application to Factory to create an AndroidViewModel class
+     */
+    private val viewModel: CatalogFragmentViewModel by lazy {
+        val activity = requireNotNull(this.activity)
+        ViewModelProvider(this,CatalogFragmentViewModel.Factory(activity.application))
+            .get(CatalogFragmentViewModel::class.java)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
