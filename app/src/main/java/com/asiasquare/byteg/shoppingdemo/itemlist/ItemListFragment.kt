ab3cd7499ed07d491b90ab2e5667b6d98414f7ee
Tran Quang Trung <tranquangtrung.vgu@gmail.com>
2021-05-27 20:40:36 +0200

    add viewBinding

--- app/src/main/java/com/asiasquare/byteg/shoppingdemo/itemlist/ItemListFragment.kt
+++ app/src/main/java/com/asiasquare/byteg/shoppingdemo/itemlist/ItemListFragment.kt
@@ -6,17 +6,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.asiasquare.byteg.shoppingdemo.R
+import com.asiasquare.byteg.shoppingdemo.databinding.FragmentItemListBinding
 
 
 class ItemListFragment : Fragment() {
 
+    private var _binding : FragmentItemListBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        _binding = FragmentItemListBinding.inflate(inflater,container,false)
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_item_list, container, false)
+        return binding.root
     }
 
 
